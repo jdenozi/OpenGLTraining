90ae982da23b1b2268afefa12f70af3712809f1a
Julien Denozi <denozi.j@gmail.com>
2023-01-20 08:21:33 +0100

    Init repo

--- src/01-triangles-IMPL/01-triangles-IMPL.cpp
+++ src/01-triangles-IMPL/01-triangles-IMPL.cpp
@@ -7,25 +7,25 @@
 #include "vgl.h"
 #include "LoadShaders.h"
 
-enum VAO_IDs { Triangles, NumVAOs };
-enum Buffer_IDs { ArrayBuffer, NumBuffers };
-enum Attrib_IDs { vPosition = 0 };
-
-GLuint  VAOs[NumVAOs];
-GLuint  Buffers[NumBuffers];
-
-const GLuint  NumVertices = 6;
 
 //----------------------------------------------------------------------------
 //
 // init
 //
 
+GLuint VAOs[1];
+GLuint VBOs[1];
+
 void init( void )
 {
-    glGenVertexArrays( NumVAOs, VAOs ); //Vertex array object 1 VAO qui est GLUInt[1], Generate a name for a new array.
+    glGenVertexArrays( 1, VAOs ); //Vertex array object 1 VAO qui est GLUInt[1], Generate a name for a new array.
 
-    glBindVertexArray( VAOs[Triangles] ); // Make the new array active, creating it if necessary.
+    glBindVertexArray( VAOs[0] ); // Make the new array active, creating it if necessary.
+
+
+    glCreateBuffers( 1, VBOs ); // Generate a name for a new buffer.
+    glBindBuffer( GL_ARRAY_BUFFER, VBOs[0] ); // // Make the new buffer active, creating it if necessary.
+
 
     GLfloat vertices[3][2] = {
             {1, 0.5f},
@@ -33,25 +33,23 @@ void init( void )
             {0.5f, -1}
     };
 
-    glCreateBuffers( NumBuffers, Buffers ); // Generate a name for a new buffer.
-    glBindBuffer( GL_ARRAY_BUFFER, Buffers[ArrayBuffer] ); // // Make the new buffer active, creating it if necessary.
     glBufferStorage( GL_ARRAY_BUFFER, sizeof(vertices), vertices, 0); // creates and initializes a buffer object's immutable data store
 
     ShaderInfo  shaders[] =
-    {
-        { GL_VERTEX_SHADER, "media/shaders/triangles/triangles.vert" },
-        { GL_FRAGMENT_SHADER, "media/shaders/triangles/triangles.frag" },
-        { GL_NONE, NULL }
-    };
+            {
+                    { GL_VERTEX_SHADER, "media/shaders/triangles/triangles.vert" },
+                    { GL_FRAGMENT_SHADER, "media/shaders/triangles/triangles.frag" },
+                    { GL_NONE, NULL }
+            };
 
     GLuint program = LoadShaders( shaders );
     glUseProgram( program );
-    // Attach the active buffer to the active array,
-    // as an array of vectors with 2 floats each
-    glVertexAttribPointer( vPosition, 2, GL_FLOAT,
+
+    glVertexAttribPointer( 0, 2,  GL_FLOAT,
                            GL_FALSE, 0, BUFFER_OFFSET(0) );
     // Enable the vertex attribute
-    glEnableVertexAttribArray( vPosition );
+    glEnableVertexAttribArray( 0 );
+
 }
 
 //----------------------------------------------------------------------------
@@ -62,12 +60,11 @@ void init( void )
 void
 display( void )
 {
-    static const float black[] = { 0.07f, 0.1f, 0.0f, 0.0f };
+    static const float black[] = { 0.0f, 0.0f, 0.0f, 0.0f };
 
     glClearBufferfv(GL_COLOR, 0, black);
-
-    glBindVertexArray( VAOs[Triangles] );
-    glDrawArrays( GL_TRIANGLES, 0, NumVertices );
+    glBindVertexArray(VAOs[0]);
+    glDrawArrays( GL_TRIANGLES, 0, 1 );
 }
 
 //----------------------------------------------------------------------------
